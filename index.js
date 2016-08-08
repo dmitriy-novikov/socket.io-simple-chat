5c491df0c7a0147e8527c2cb52c5ea3cdd8b258e
Dmitriy <me@dmnv.ru>
2016-08-08 18:50:03 +0300

    indents, css fix

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 app.get('/style.css', function(req, res){
-  res.sendFile(__dirname + '/style.css');
+	res.sendFile(__dirname + '/style.css');
 });
 
 io.on('connection', function(socket){
-  io.emit('system message', ' *** Кто-то добавился');
-  socket.on('disconnect', function(){
-    io.emit('system message', ' *** Кто-то('+socket.id+') вышел из чата');
-  });
-  socket.on('chat message', function(msg){
-    socket.broadcast.emit('chat message', msg);
-  });
+		io.emit('system message', ' *** Кто-то добавился');
+		socket.on('disconnect', function(){
+		io.emit('system message', ' *** Кто-то('+socket.id+') вышел из чата');
+	});
+	socket.on('chat message', function(msg){
+		socket.broadcast.emit('chat message', msg);
+	});
 });
 
 
 
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
