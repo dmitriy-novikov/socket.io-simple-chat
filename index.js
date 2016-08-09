0d34ae4e3a47927d0c466b307a65ac65038f969e
Dmitriy <me@dmnv.ru>
2016-08-09 15:23:57 +0300

    custom usernames support

--- index.js
+++ index.js
@@ -9,13 +9,26 @@ app.get('/style.css', function(req, res){
 	res.sendFile(__dirname + '/style.css');
 });
 
+var users = [];
+
 io.on('connection', function(socket){
 		io.emit('system message', ' *** Кто-то добавился');
 		socket.on('disconnect', function(){
 		io.emit('system message', ' *** Кто-то('+socket.id+') вышел из чата');
 	});
 	socket.on('chat message', function(msg){
-		socket.broadcast.emit('chat message', msg);
+		var mess = {
+			user : users[socket.id],
+			msg : msg
+		};
+		
+
+
+		socket.broadcast.emit('chat message', mess);
+
+	});
+	socket.on('name user', function(name){
+		users[socket.id] = name;
 	});
 });
 
